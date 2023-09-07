8acc73ff16ecfcdd6c263cadfe88cb727c7cde67
Taras21071988 <tsasojlov@icloud.com>
2023-09-07 11:25:08 +0300

    feedback block layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuMobile = document.querySelector(".header__menu-list");
 
-menuBtn.addEventListener('click',()=>{
-    menuMobile.classList.toggle('menu--open')
-})
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
