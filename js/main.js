e18f59ac3ca5c144d7bcc44d78984ae878f5a1a2
Taras21071988 <tsasojlov@icloud.com>
2023-09-12 11:55:20 +0300

    writing styles for smooth operation of accordeon

--- js/main.js
+++ js/main.js
@@ -40,6 +40,12 @@ accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
     const currentText =
       accordeonTitle.parentElement.querySelector(".accordeon__text");
     accordeonTitle.classList.toggle("accordeon__title--active");
-    currentText.classList.toggle('accordeon__text--visible')
+    currentText.classList.toggle("accordeon__text--visible");
+
+    if (currentText.classList.contains("accordeon__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
   });
 });
